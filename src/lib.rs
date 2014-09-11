a1658b93a78b9c3f7f5f99aa3c6ca2cce6a8d984
Jonathan Reem <jonathan.reem@gmail.com>
2014-09-10 20:36:19 -0700

    (doc) Documented Actor.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #![feature(macro_rules)]
 #![license = "MIT"]
-//#![deny(missing_doc)]
+#![deny(missing_doc)]
 #![deny(warnings)]
 
 //! Crate comment goes here
@@ -21,6 +21,9 @@ macro_rules! distributor(
     }}
 )
 
+/// A collection of channels that is used to communicate with running Actors.
+///
+/// It is stored in task local storage.
 pub struct Distributor {
     // ChannelKey -> Channel
     channels: TypeMap
@@ -37,25 +40,22 @@ enum ChannelKey<S: Send, R: Send, A: Actor<S, R>> {}
 
 impl<S: Send, R: Send, A: Actor<S, R>> Assoc<Channel<R, S>> for ChannelKey<S, R, A> {}
 
+/// Adds the register method to `Arc<Mutex<Distributor>>`
 pub trait Register {
-    fn register<A, S, R>(&self, worker: A)
+    /// Register an Actor to be managed by this Distributor.
+    fn register<A, S, R>(&self, actor: A)
     where A: Actor<S, R>,
           S: Send, R: Send;
 }
 
 impl Register for Arc<Mutex<Distributor>> {
-    fn register<A, S, R>(&self, worker: A)
+    fn register<A, S, R>(&self, actor: A)
     where A: Actor<S, R>,
           S: Send, R: Send {
-        let (worker_sender, distributor_receiver) = channel();
-        let (distributor_sender, worker_receiver) = channel();
+        let (actor_sender, distributor_receiver) = channel();
+        let (distributor_sender, actor_receiver) = channel();
 
-        let new = self.clone();
-        // Start the worker in a different thread so it blocks.
-        spawn(proc() {
-            LocalDistributor.replace(Some(new));
-            worker.start(worker_sender, worker_receiver);
-        });
+        spawn(proc() { actor.start(actor_sender, actor_receiver); });
 
         let dist_channel = Channel {
             sender: distributor_sender,
@@ -67,48 +67,75 @@ impl Register for Arc<Mutex<Distributor>> {
 }
 
 impl Distributor {
+    /// Create a new Distributor, putting it in task local storage.
     pub fn new() -> Arc<Mutex<Distributor>> {
         let dist = Arc::new(Mutex::new(Distributor { channels: TypeMap::new() }));
         LocalDistributor.replace(Some(dist.clone()));
         dist
     }
+
     // Does not need mutability, but can't be safe without exclusive access.
-    fn direct<A, S, R>(&mut self, message: R) -> Option<S>
+    #[inline]
+    fn direct<A, S, R>(&mut self, message: R) -> S
     where A: Actor<S, R>,
           S: Send, R: Send {
-        match self.channels.find::<ChannelKey<S, R, A>, Channel<R, S>>() {
-            Some(channel) => {
-                channel.sender.send(message);
-                Some(channel.receiver.recv())
-            },
-            None => None
-        }
+        let channel = self.channels.find::<ChannelKey<S, R, A>, Channel<R, S>>()
+            .expect("Tried to send a Message to a non-existent Actor.");
+        channel.sender.send(message);
+        channel.receiver.recv()
     }
 }
 
+/// An Actor, which runs independently in its own thread.
 pub trait Actor<Sends, Receives>: Send {
+    /// Start an Actor with a Sender and Receiver.
+    ///
+    /// This method should be blocking, and will be run in its own thread.
+    /// One way to structure this method is to iterate over the Receiver values.
     fn start(self, Sender<Sends>, Receiver<Receives>);
 }
 
+/// A message which can be sent to an Actor from any thread with a Distributor.
 pub struct Message<Data: Send> {
     data: Data
 }
 
 impl<R: Send> Message<R> {
-    pub fn new(data: R) -> Message<R> {
-        Message {
-            data: data
-        }
-    }
+    /// Create a new Message.
+    #[inline]
+    pub fn new(data: R) -> Message<R> { Message { data: data } }
 
-    pub fn send<A: Actor<S, R>, S: Send>(self) -> Option<S> {
+    /// Send this message to the indicated Actor.
+    ///
+    /// This method will block the current task until the other Actor
+    /// sends back a Response.
+    ///
+    /// ## Failure
+    ///
+    /// Will fail if there is no such Actor registered with the
+    /// Distributor or if there if called in a task with no Distributor
+    /// in the task local storage.
+    #[inline]
+    pub fn send<A: Actor<S, R>, S: Send>(self) -> S {
         (distributor!()).lock().direct::<A, S, R>(self.data)
     }
 }
 
+/// If any Actor has to spawn a new thread internally, it should use
+/// use this instead of the prelude spawn, as this spawn will also
+/// put the Distributor in task local storage in the new thread, allowing
+/// that thread to send messages.
+///
+/// ## Failure
+///
+/// Will fail if there is no Distributor in the task local storage
+/// of the current task.
+#[inline]
 pub fn spawn(todo: proc(): Send) {
+    use std::task;
+
     let new = (distributor!()).clone();
-    spawn(proc() {
+    task::spawn(proc() {
         LocalDistributor.replace(Some(new));
         todo()
     });
